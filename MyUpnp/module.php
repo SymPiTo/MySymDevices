075f45b6f4326cf7f6fe40bb488295ae7e36e444
Your NameSymPiTo <pito@online.de>
2019-11-01 11:55:19 +0100

    ss

--- MyUpnp/module.php
+++ MyUpnp/module.php
@@ -1611,6 +1611,7 @@ class MyUpnp extends IPSModule {
         foreach ($container as $value) {
             $playlistname = $value['no'];
             $No = intval($playlistname);
+            print_r("IndexNr.: ".$No);
             $mediaDB->media[$No]->playlistname = $playlistname;
             $mediaDB->media[$No]->id = $value['id'];
             $mediaDB->media[$No]->album = $value['album'];
@@ -1633,7 +1634,7 @@ class MyUpnp extends IPSModule {
                 $BrowseResult = $this->ContentDirectory_Browse ($ServerIP, $ServerPort, $Kernel, $ServerContentDirectory, $ObjectID, $BrowseFlag, $Filter, $StartingIndex, $RequestedCount, $SortCriteria);
                 $Result_xml = $BrowseResult['Result'] ;
                 $liste = $this->BrowseList($Result_xml);
-                print_r($liste[0]); 
+                 
                 $cover = $liste[0]['albumArtURI'];
                 $mediaDB->media[$No]->icon = $cover;
                 $total = $BrowseResult['TotalMatches'];
