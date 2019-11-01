a67b5de7982f7f687d82b19231b6e2dbc491b85f
Your NameSymPiTo <pito@online.de>
2019-11-01 09:47:35 +0100

    ss

--- MyUpnp/UpnpClassTrait.php
+++ MyUpnp/UpnpClassTrait.php
@@ -682,7 +682,7 @@ trait upnp {
 	      	if(isset($e->detail->UPnPError->errorCode)){
 	        	$errorCode   = $e->detail->UPnPError->errorCode;
 	        	throw new Exception("Error during Soap Call: ".$faultstring." ".$faultcode." ".$errorCode." (".$this->resolveErrorCode($path,$errorCode).")");
-	      	
+						$this->Meldung($this->resolveErrorCode($path,$errorCode));
                         return false;
                 }
 			else{
@@ -861,7 +861,26 @@ trait upnp {
         return $message;
         }
         
-        
+    //*****************************************************************************
+	/*  - Sammlung / Tools
+    /*//////////////////////////////////////////////////////////////////////////////
+    //
+	//*****************************************************************************
+	/* Function: Meldung($Meldung)
+    ...............................................................................
+    	Anzeige im Meldungsfenster (Textbox) ObjectID 11701
+    ...............................................................................
+        Parameters: 
+            $Meldung - Ausgabetext in Debug Fenster
+ 	--------------------------------------------------------------------------------
+		Returns: 
+    -----------------------------------------------------------------------------
+    	Status: 
+    *****************************************************************************/
+        Protected function Meldung(string $Meldung){
+            $this->SendDebug('UPNP: ', $Meldung, 0);
+            setvalue($this->GetIDForIdent("upnp_Message"), $Meldung);
+        }  
 
 }
 
