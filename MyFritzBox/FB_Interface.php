6c10c3a718c10bce92f4bdeef9e27e6c5c0d53b1
GitIPS <torsten.beck@@onlinehome.de>
2020-08-03 20:00:14 +0200

    fb

---
+++ MyFritzBox/FB_Interface.php
@@ -0,0 +1,390 @@
+<?php
+
+trait FB_soap  
+{
+          
+	/*//////////////////////////////////////////////////////////////////////////////
+	2018-02´5-01 (TESTED-OK)
+	--------------------------------------------------------------------------------
+	Funktion 	:	Gibt den Lautstärke Wert zurück
+						 
+	Befehl		:	GetVolume($channel = 'Master')  
+	--------------------------------------------------------------------------------
+	Soap / upnp-command
+	--------------------------------------------------------------------------------
+	Parameter:		 $channel = 'Master'
+					
+	Rückgabewert: 	 Integer Wert 0 - x 
+	//////////////////////////////////////////////////////////////////////////////*/
+	  public function GetDHCP_Server()
+	  {
+	    return (int)$this->processSoapCall(
+						"/upnp/control/lanhostconfigmgm",
+
+					    "urn:dslforum-org:service-1-0",
+
+					    "GetInfo",
+
+					       array(
+
+						      new SoapParam("0"     ,"InstanceID"),
+
+						      new SoapParam($channel,"Channel"   )
+
+							)
+		);
+	  }
+
+
+
+# Daten eines Dienste aus der FritzBox lesen
+# $fbroot = 'http://'.FB_HOST.':'.FB_PORT; //Adresse + Port (immer 49000)
+# $desc = "tr64desc.xml"; //Hier sind die Infos über die Dienste enthalten
+# SCPD = "lanhostconfigmgmSCPD.xml";  // Hier sind die Infos über die Funktionen und Parameter/Variablen enthalten, sowie die Definition der Variablen Typen.
+# $action="GetInfo";  //Diese Funktion soll ausgeführt werden
+public function FB_getServiceData($fbroot,$descXML,$SCPD)
+{
+	global $_IPS;
+
+	$xml = @simplexml_load_file($fbroot.'/'.$descXML);
+	if ($xml === false)
+	{
+		if ($_IPS['SENDER'] == "WebFront") echo "Not found: ".$descXML.PHP_EOL;
+		else IPS_LogMessage("FritzBox","ERROR: Not found: ".$descXML);
+		return false;
+	}
+	$xml->registerXPathNamespace('fb', $xml->getNameSpaces(false)[""]);
+	$xmlservice = $xml->xpath("//fb:service[fb:SCPDURL='/".$SCPD."']");
+	$service['uri'] = (string)$xmlservice[0]->serviceType;
+	$service['location'] =$fbroot.(string)$xmlservice[0]->controlURL;
+	$service['SCPDURL'] =trim((string)$xmlservice[0]->SCPDURL,"/");
+	return $service;
+}
+ 
+
+	/*//////////////////////////////////////////////////////////////////////////////
+	2018-02´5-01 (TESTED-OK)
+	--------------------------------------------------------------------------------
+	Funktion 	:	Sub Routine für Soap Call. Übertragt upnp Protokoll im Netzwerk
+						 
+	Befehl		:	processSoapCall($path,$uri,$action,$parameter)
+	--------------------------------------------------------------------------------
+	Soap / upnp-command
+	--------------------------------------------------------------------------------
+	Parameter:  $path  
+				$uri		 
+ 				$action
+				$parameter
+				
+	Rückgabewert: 	Fehler Code
+	//////////////////////////////////////////////////////////////////////////////*/	
+	  Protected function processSoapCall(string $path, string $uri,string $action, array $parameter)
+	  {
+	    try{
+		$ip = $this->ReadPropertyString('IPAddress');
+	      $client     = new SoapClient(null, array("location"   => "http://".$ip.":49000".$path,
+
+						       "uri"        => $uri,
+
+						       "trace"      => true ));
+
+	      return $client->__soapCall($action,$parameter);
+	    }catch(Exception $e){
+	      $faultstring = $e->faultstring;
+	      $faultcode   = $e->faultcode;
+	      if(isset($e->detail->UPnPError->errorCode)){
+		$errorCode   = $e->detail->UPnPError->errorCode;
+		throw new Exception("Error during Soap Call: ".$faultstring." ".$faultcode." ".$errorCode." (".$this->resoveErrorCode($path,$errorCode).")");
+	      }else{
+
+		throw new Exception("Error during Soap Call: ".$faultstring." ".$faultcode);
+
+	      }
+
+	    }
+
+	  }
+
+
+
+	/*//////////////////////////////////////////////////////////////////////////////
+	2018-02´5-01 (TESTED-OK)
+	--------------------------------------------------------------------------------
+	Funktion 	:	Sub Routine für Soap Call. Error Handling
+						 
+	Befehl		:	processSoapCall($path,$uri,$action,$parameter)
+	--------------------------------------------------------------------------------
+	Soap / upnp-command
+	--------------------------------------------------------------------------------
+	Parameter:  $path  
+				$uri		 
+ 				$action
+				$parameter
+				
+	Rückgabewert: 	Fehler Code
+	//////////////////////////////////////////////////////////////////////////////*/
+	  private function resolveErrorCode(string $path, $errorCode)
+
+	  {
+
+	   $errorList = array( "/AVTransport/ctrl"      => array(
+
+										   "701" => "ERROR_AV_UPNP_AVT_INVALID_TRANSITION",
+
+										   "702" => "ERROR_AV_UPNP_AVT_NO_CONTENTS",
+
+										   "703" => "ERROR_AV_UPNP_AVT_READ_ERROR",
+
+										   "704" => "ERROR_AV_UPNP_AVT_UNSUPPORTED_PLAY_FORMAT",
+
+										   "705" => "ERROR_AV_UPNP_AVT_TRANSPORT_LOCKED",
+
+										   "706" => "ERROR_AV_UPNP_AVT_WRITE_ERROR",
+
+										   "707" => "ERROR_AV_UPNP_AVT_PROTECTED_MEDIA",
+
+										   "708" => "ERROR_AV_UPNP_AVT_UNSUPPORTED_REC_FORMAT",
+
+										   "709" => "ERROR_AV_UPNP_AVT_FULL_MEDIA",
+
+										   "710" => "ERROR_AV_UPNP_AVT_UNSUPPORTED_SEEK_MODE",
+
+										   "711" => "ERROR_AV_UPNP_AVT_ILLEGAL_SEEK_TARGET",
+
+										   "712" => "ERROR_AV_UPNP_AVT_UNSUPPORTED_PLAY_MODE",
+
+										   "713" => "ERROR_AV_UPNP_AVT_UNSUPPORTED_REC_QUALITY",
+
+										   "714" => "ERROR_AV_UPNP_AVT_ILLEGAL_MIME",
+
+										   "715" => "ERROR_AV_UPNP_AVT_CONTENT_BUSY",
+
+										   "716" => "ERROR_AV_UPNP_AVT_RESOURCE_NOT_FOUND",
+
+										   "717" => "ERROR_AV_UPNP_AVT_UNSUPPORTED_PLAY_SPEED",
+
+										   "718" => "ERROR_AV_UPNP_AVT_INVALID_INSTANCE_ID"
+
+										 ),
+
+			       "/RenderingControl/ctrl" => array(
+
+										   "701" => "ERROR_AV_UPNP_RC_INVALID_PRESET_NAME",
+
+										   "702" => "ERROR_AV_UPNP_RC_INVALID_INSTANCE_ID"
+
+										 ),
+
+			       "/ContentDirectory/ctrl"   => array(
+
+										   "701" => "ERROR_AV_UPNP_CD_NO_SUCH_OBJECT",
+
+										   "702" => "ERROR_AV_UPNP_CD_INVALID_CURRENTTAGVALUE",
+
+										   "703" => "ERROR_AV_UPNP_CD_INVALID_NEWTAGVALUE",
+
+										   "704" => "ERROR_AV_UPNP_CD_REQUIRED_TAG_DELETE",
+
+										   "705" => "ERROR_AV_UPNP_CD_READONLY_TAG_UPDATE",
+
+										   "706" => "ERROR_AV_UPNP_CD_PARAMETER_NUM_MISMATCH",
+
+										   "708" => "ERROR_AV_UPNP_CD_BAD_SEARCH_CRITERIA",
+
+										   "709" => "ERROR_AV_UPNP_CD_BAD_SORT_CRITERIA",
+
+										   "710" => "ERROR_AV_UPNP_CD_NO_SUCH_CONTAINER",
+
+										   "711" => "ERROR_AV_UPNP_CD_RESTRICTED_OBJECT",
+
+										   "712" => "ERROR_AV_UPNP_CD_BAD_METADATA",
+
+										   "713" => "ERROR_AV_UPNP_CD_RESTRICTED_PARENT_OBJECT",
+
+										   "714" => "ERROR_AV_UPNP_CD_NO_SUCH_SOURCE_RESOURCE",
+
+										   "715" => "ERROR_AV_UPNP_CD_SOURCE_RESOURCE_ACCESS_DENIED",
+
+										   "716" => "ERROR_AV_UPNP_CD_TRANSFER_BUSY",
+
+										   "717" => "ERROR_AV_UPNP_CD_NO_SUCH_FILE_TRANSFER",
+
+										   "718" => "ERROR_AV_UPNP_CD_NO_SUCH_DESTINATION_RESOURCE",
+
+										   "719" => "ERROR_AV_UPNP_CD_DESTINATION_RESOURCE_ACCESS_DENIED",
+
+										   "720" => "ERROR_AV_UPNP_CD_REQUEST_FAILED"
+
+										 ) ); 
+
+
+
+	    if (isset($errorList[$path][$errorCode])){
+
+	      return $errorList[$path][$errorCode] ;
+
+	    }else{
+
+	      return "UNKNOWN";
+
+	    }
+
+	  }
+
+
+	/*//////////////////////////////////////////////////////////////////////////////
+	2018-02´5-01 (TESTED-OK)
+	-------------------------------------------------------------------------------- 	
+	Funktion 	:	sende Telnet Befehle	 	 					
+	Befehl		:	send_cmd($cmd)
+	-------------------------------------------------------------------------------------------
+		CEOL-TELNET-command
+	-------------------------------------------------------------------------------------------
+ 	Parameter:		 $cmd
+					 'MVUP'  	-> Master Volume UP
+					 'MVDOWN'	-> Master Volume DOWN
+					 
+	Rückgabewert: 	 $xml
+	//////////////////////////////////////////////////////////////////////////////*/		
+	Protected function send_cmd(string $cmd){
+ 		$ip = $this->ReadPropertyString('IPAddress');
+		$url = "http://$ip:80/goform/formiPhoneAppDirect.xml";
+		
+		$xml = $this->curl_get($url, $cmd);
+		return $xml;
+	}
+
+
+	
+	
+	/*//////////////////////////////////////////////////////////////////////////////
+	2018-02´5-01 (TESTED-OK)
+	--------------------------------------------------------------------------------
+	* 2nd Method
+	* Send a POST request using cURL 
+	* @param string $url to request
+		$host = "192.168.178.29";  		
+		$url =  "http://192.168.178.29/goform/AppCommand.xml";
+		
+	* @param array $post values to send 	=
+							$xml = "<?xml version="1.0" encoding="utf-8"?>";
+							$xml .= "<tx>";
+	 						$xml .= "<cmd id="1">SetFavoriteStation</cmd>";
+	 						$xml .= "<zone>Main</zone>";
+	 						$xml .= "<value>3</value>";
+							$xml .= "</tx>"
+		$post =  $xml;										
+	
+	* @param array $options for cURL 
+	* @return string 
+	//////////////////////////////////////////////////////////////////////////////*/	
+	Protected function curl_post($url, $post = NULL) 
+	{
+		print_r($url)."\n";
+		print_r($post)."\n";
+		$defaults = array(
+			CURLOPT_POST => 1,
+			//CURLOPT_HEADER => array('Content-Type: text/xml'),
+			CURLOPT_HEADER => 1,
+			// set url
+			CURLOPT_URL => $url,
+			CURLOPT_FRESH_CONNECT => 1,
+			//return the transfer as a string
+			CURLOPT_RETURNTRANSFER => 1,
+			//CURLOPT_FORBID_REUSE => 1,
+			CURLOPT_TIMEOUT => 5,
+			//CURLOPT_POSTFIELDS => http_build_query($post)
+			//CURLOPT_POSTFIELDS => $post
+			);
+		// create curl resource
+		$ch = curl_init();
+		curl_setopt_array($ch, ($defaults));
+		
+		
+		if( ! $result = curl_exec($ch))
+		{
+			trigger_error(curl_error($ch));
+		}
+		// close curl resource to free up system resources
+		curl_close($ch);
+		return $result;
+		} 
+	
+	/*//////////////////////////////////////////////////////////////////////////////
+	2018-02´5-01 (TESTED-OK)
+	--------------------------------------------------------------------------------
+	* 1st Method
+	* Send a GET request using cURL 
+	* @param string $url to request 
+	* @param array $get values to send 
+	* @param array $options for cURL 
+	* @return string 
+	
+	//////////////////////////////////////////////////////////////////////////////*/	
+	Protected function curl_get($url, $get)
+	{
+		//print_r($get);
+		//$test = $url. (strpos($url, '?') === FALSE ? '?' : ''). http_build_query($get);
+		//$test = $url. (strpos($url, '?') === FALSE ? '?' : ''). $get;
+		//print_r($test);
+		//$ret = Message($test);
+		$defaults = array(
+			//CURLOPT_URL => $url. (strpos($url, '?') === FALSE ? '?' : ''). http_build_query($get),
+			CURLOPT_URL => $url. (strpos($url, '?') === FALSE ? '?' : ''). $get,
+			CURLOPT_HEADER => 0,
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_TIMEOUT => 4
+		);
+		
+		$ch = curl_init();
+		curl_setopt_array($ch, ($defaults));
+		
+		$result = curl_exec($ch);
+		
+		
+		$error = curl_error($ch);
+		//print_r($result);
+		
+		//if( ! $result = curl_exec($ch))
+		//{
+			//trigger_error('Curl-Fehler:'.curl_error($ch));
+		//}
+		$errno = curl_errno($ch);
+	
+		//	print_r($errno);
+		
+		curl_close($ch);
+		return $result;
+	}
+        
+            
+        Public function TelnetCeol(string $command, string $value) {
+            $ip = $this->ReadPropertyString('IPAddress');
+            $socket = fsockopen($ip, 23, $errno, $errstr); 
+
+            if($socket) 
+            { 
+                //echo "Connected <br /><br />"; 
+            } 
+            else 
+            { 
+                //echo "Connection failed!<br /><br />"; 
+            } 
+            $cmd = $command.$value.chr(13);
+            fputs($socket, $cmd); 
+            //$buffer = ""; 
+            //while(!feof($socket)) 
+            //{ 
+              //  $buffer .=fgets($socket, 4096); 
+            //} 
+
+            //print_r($buffer); 
+            //echo "<br /><br /><br />"; 
+            //var_dump($buffer); 
+
+            fclose($socket); 
+        }
+} //Ende der Klasse
+
+
+?>
---
+++ MyFritzBox/module.php
@@ -0,0 +1,366 @@
+<?php
+/***************************************************************************
+ * Title: _TITEL_
+ *
+ * Author: _AUTOR_
+ * 
+ * GITHUB: <https://github.com/SymPiTo/MySymApps/tree/master/_TITEL_>
+ * 
+ * Version: _VERSION_
+ *************************************************************************** */
+require_once(__DIR__ . "/FB_Interface.php");
+//require_once __DIR__ . '/../libs/_HELPERCLASS_';  // diverse Klassen
+
+class MyFB extends IPSModule {
+
+    use FB_soap;
+/* 
+___________________________________________________________________________ 
+    Section: Internal Modul Funtions
+    Die folgenden Funktionen sind Standard Funktionen zur Modul Erstellung.
+___________________________________________________________________________ 
+    */
+    /* 
+    ------------------------------------------------------------ 
+        Function: Create  
+        Create() Wird ausgeführt, beim anlegen der Instanz.
+    -------------------------------------------------------------
+    */
+    public function Create() {
+    //Never delete this line!
+        parent::Create();
+
+        //Register Properties from form.json
+        $this->RegisterPropertyBoolean("active", false);
+
+        //$this->ReadPropertyFloat("NAME", 0.0);
+
+        $this->ReadPropertyInteger("UpdateInterval", 0);
+
+        $this->ReadPropertyString("IPAddress", "ip");
+
+        // Register Profiles
+        $this->RegisterProfiles();
+
+        //Register Variables
+        //$variablenID = $this->RegisterVariableBoolean ($Ident, $Name, $Profil, $Position);
+        //IPS_SetInfo ($variablenID, "WSS");
+        //IPS_SetHidden($variablenID, true); //Objekt verstecken
+
+        //$variablenID = $this->RegisterVariableFloat ($Ident, $Name, $Profil, $Position);
+        //IPS_SetInfo ($variablenID, "WSS");
+        //IPS_SetHidden($variablenID, true); //Objekt verstecken
+
+        //$variablenID = $this->RegisterPropertyInteger ($Name, $Standardwert);
+        //IPS_SetInfo ($variablenID, "WSS");
+        //IPS_SetHidden($variablenID, true); //Objekt verstecken
+
+        //$variablenID = $this->RegisterPropertyString ($Name, $Standardwert);
+        //IPS_SetInfo ($variablenID, "WSS");
+        //IPS_SetHidden($variablenID, true); //Objekt verstecken
+
+        //Register Timer
+        //$this->RegisterTimer('Name', 0, '_PREFIX__Scriptname($_IPS[\'TARGET\']);');
+
+    
+
+        //Webfront Actions setzen
+        //$this->EnableAction("IDENT der registrierten Variable");
+    } //Function: Create End
+    /* 
+    ------------------------------------------------------------ 
+        Function: ApplyChanges  
+        ApplyChanges() Wird ausgeführt, beim anlegen der Instanz.
+        und beim ändern der Parameter in der Form
+    -------------------------------------------------------------
+    */
+    public function ApplyChanges(){
+        //Never delete this line!
+        parent::ApplyChanges();
+
+        //Messages registrieren
+        $this->RegisterMessage(0, IPS_KERNELSTARTED);
+        $this->RegisterMessage($this->InstanceID, FM_CONNECT);
+        $this->RegisterMessage($this->InstanceID, FM_DISCONNECT);
+
+        if($this->ReadPropertyBoolean("active")){
+ 
+        }
+        else {
+            //Timer ausschalten
+            //$this->SetTimerInterval("Name", 0);
+        }                   
+    } //Function: ApplyChanges  End
+    /* 
+    ------------------------------------------------------------ 
+        Function: Destroy  
+            Destroy() wird beim löschen der Instanz 
+            und update der Module aufgerufen
+    -------------------------------------------------------------
+    */
+    public function Destroy() {
+        //Never delete this line!
+        parent::Destroy();
+    } //Function: Destroy End
+    /* 
+    ------------------------------------------------------------ 
+        Function: RequestAction  
+            RequestAction() wird von schaltbaren Variablen 
+            aufgerufen.
+    -------------------------------------------------------------
+    */ 
+    public function RequestAction($Ident, $Value) {     
+        switch($Ident) {
+            case "IDENT_Variable":
+                if ($Value == true){ 
+
+                }
+                else {
+
+                }
+                break;
+            default:
+                throw new Exception("Invalid Ident");
+            }
+    } //Function: RequestAction End
+    /* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+/* 
+_____________________________________________________________________________________________________________________
+    Section: Public Funtions
+    Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+    Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wie folgt zur Verfügung gestellt:
+    
+    FSSC_XYFunktion($Instance_id, ... );
+________________________________________________________________________________________________________________________ 
+*/
+    //-----------------------------------------------------------------------------
+    /* Function: xxxx
+    ...............................................................................
+    Beschreibung
+    ...............................................................................
+    Parameters: 
+        none
+    ...............................................................................
+    Returns:    
+        none
+    ------------------------------------------------------------------------------  */
+    public function xxxx(){
+       
+    }  //xxxx End
+
+/* 
+_______________________________________________________________________
+    Section: Private Funtions
+    Die folgenden Funktionen sind nur zur internen Verwendung verfügbar
+    Hilfsfunktionen
+______________________________________________________________________
+*/ 
+    /* ----------------------------------------------------------------------------
+    Function: createProfile
+    ...............................................................................
+    Erstellt ein neues Profil und ordnet es einer Variablen zu.
+    ...............................................................................
+    Parameters: 
+        $Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Vartype, $VarIdent, $Assoc
+     * $Vartype: 0 boolean, 1 int, 2 float, 3 string,
+     * $Assoc: array mit statustexte
+     *         $assoc[0] = "aus";
+     *         $assoc[1] = "ein";
+     *  
+    ..............................................................................
+    Returns:   
+        none
+    ------------------------------------------------------------------------------- */
+    protected function createProfile(string $Name, int $Vartype, $Assoc, $Icon,  $Prefix,  $Suffix,   $MinValue,   $MaxValue,  $StepSize,  $Digits){
+        if (!IPS_VariableProfileExists($Name)) {
+            IPS_CreateVariableProfile($Name, $Vartype); // 0 boolean, 1 int, 2 float, 3 string,
+            if(!is_Null($Icon)){
+                IPS_SetVariableProfileIcon($Name, $Icon);
+            }
+            if(!is_Null($Prefix)){
+                IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
+            }
+            if(!is_Null($Digits)){
+                IPS_SetVariableProfileDigits($Name, $Digits); //  Nachkommastellen
+            }
+            if(!is_Null($MinValue)){
+                IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
+            }
+            if(!is_Null($Assoc)){
+                foreach ($Assoc as $key => $data) {
+                    if(is_null($data['icon'])){$data['icon'] = "";}; 
+                    if(is_null($data['color'])){$data['color'] = "";}; 
+                    IPS_SetVariableProfileAssociation($Name, $data['value'], $data['text'], $data['icon'], $data['color']);  
+                }
+            }
+        } 
+        else {
+            $profile = IPS_GetVariableProfile($Name);
+            if ($profile['ProfileType'] != $Vartype){
+                // $this->SendDebug("Alarm.Reset:", "Variable profile type does not match for profile " . $Name, 0);
+            }
+        }
+    }   //Function: createProfile End
+    
+    
+    
+    /* ----------------------------------------------------------------------------
+     Function: RegisterProfiles()
+    ...............................................................................
+        Profile für Variable anlegen falls nicht schon vorhanden
+    ...............................................................................
+    Parameters: 
+        $Vartype => 0 boolean, 1 int, 2 float, 3 string
+    ..............................................................................
+    Returns:   
+        $ipsversion
+    ------------------------------------------------------------------------------- */
+    protected function RegisterProfiles(){
+       /*   Profile "UPNP_Browse";   
+        $Assoc[0]['value'] = 0;
+        $Assoc[1]['value'] = 1;
+        $Assoc[2]['value'] = 2;
+        $Assoc[3]['value'] = 3;
+        $Assoc[0]['text'] = "Up";
+        $Assoc[1]['text'] = "Select";
+        $Assoc[2]['text'] = "Left";
+        $Assoc[3]['text'] = "Right";
+        $Assoc[0]['icon'] = NULL;
+        $Assoc[1]['icon'] = NULL;
+        $Assoc[2]['icon'] = NULL;
+        $Assoc[3]['icon'] = NULL;
+        $Assoc[0]['color'] = NULL;
+        $Assoc[1]['color'] = NULL;
+        $Assoc[2]['color'] = NULL;
+        $Assoc[3]['color'] = NULL;
+        $Name = "UPNP_Browse";
+        $Vartype = 1;
+        $Icon = NULL;
+        $Prefix = NULL;
+        $Suffix = NULL;
+        $MinValue = 0;
+        $MaxValue = 3;
+        $StepSize = 1;
+        $Digits = 0;
+        if (!IPS_VariableProfileExists($Name)){
+            $this->createProfile($Name, $Vartype,  $Assoc, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits);  
+        }
+        */
+    } //Function: RegisterProfiles End
+
+    /** Wird ausgeführt wenn der Kernel hochgefahren wurde. */
+    protected function KernelReady(){
+        $this->ApplyChanges();
+    }
+    /* ----------------------------------------------------------------------------
+     Function: GetIPSVersion()
+    ...............................................................................
+        gibt die eingestezte IPS Version zurück
+    ...............................................................................
+    Parameters: 
+        none
+    ..............................................................................
+    Returns:   
+        $ipsversion
+    ------------------------------------------------------------------------------- */
+    protected function GetIPSVersion(){
+        $ipsversion = floatval(IPS_GetKernelVersion());
+        if ($ipsversion < 4.1) {    // 4.0
+            $ipsversion = 0;
+        } elseif ($ipsversion >= 4.1 && $ipsversion < 4.2){ // 4.1
+            $ipsversion = 1;
+        } elseif ($ipsversion >= 4.2 && $ipsversion < 4.3){ // 4.2
+            $ipsversion = 2;
+        } elseif ($ipsversion >= 4.3 && $ipsversion < 4.4){ // 4.3
+            $ipsversion = 3;
+        } elseif ($ipsversion >= 4.4 && $ipsversion < 5){ // 4.4
+            $ipsversion = 4;
+        } else {  // 5
+            $ipsversion = 5;
+        }
+        return $ipsversion;
+    } //Function: GetIPSVersion End
+    /* --------------------------------------------------------------------------- 
+    Function: RegisterEvent
+    ...............................................................................
+    legt einen Event an wenn nicht schon vorhanden
+      Beispiel:
+      ("Wochenplan", "SwitchTimeEvent".$this->InstanceID, 2, $this->InstanceID, 20);  
+      ...............................................................................
+    Parameters: 
+      $Name        -   Name des Events
+      $Ident       -   Ident Name des Events
+      $Typ         -   Typ des Events (0=ausgelöstes 1=cyclic 2=Wochenplan)
+      $Parent      -   ID des Parents
+      $Position    -   Position der Instanz
+    ...............................................................................
+    Returns:    
+        none
+    -------------------------------------------------------------------------------*/
+    private function RegisterEvent($Name, $Ident, $Typ, $Parent, $Position){
+        $eid = @$this->GetIDForIdent($Ident);
+        if($eid === false) {
+                $eid = 0;
+        } elseif(IPS_GetEvent($eid)["EventType"] <> $Typ) {
+                IPS_DeleteEvent($eid);
+                $eid = 0;
+        }
+        //we need to create one
+        if ($eid == 0) {
+                $EventID = IPS_CreateEvent($Typ);
+                IPS_SetParent($EventID, $Parent);
+                IPS_SetIdent($EventID, $Ident);
+                IPS_SetName($EventID, $Name);
+                IPS_SetPosition($EventID, $Position);
+                IPS_SetEventActive($EventID, false);  
+        }
+    } //Function: RegisterEvent End
+
+} //end Class
+
+
