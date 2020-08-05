80d6f0b88a0907ef86c3193d69e977ecc74b4f9c
GitIPS <torsten.beck@@onlinehome.de>
2020-08-05 18:38:58 +0200

    ff

--- MyFritzBox/FB_Interface.php
+++ MyFritzBox/FB_Interface.php
@@ -98,7 +98,7 @@ public function MygetServiceData($fbroot,$descXML,$SCPD)
 
 							));
 
-	      return $client->GetSpecificHostEntry($parameter);
+	      return $client->__soapCall($action,$parameter);
 	    }catch(Exception $e){
 	      $faultstring = $e->faultstring;
 	      $faultcode   = $e->faultcode;
