a9eeaaa5c7c3c2250e1193a019ef06b2d93edbc7
GitIPS <torsten.beck@@onlinehome.de>
2021-12-29 22:27:31 +0100

    fdsf

--- MyUpnp/module.php
+++ MyUpnp/module.php
@@ -866,7 +866,7 @@ class MyUpnp extends IPSModule {
         $this->SendDebug("PLAY ", "Status auf PLAY setzten", 0); 
 
         $tracks = $this->GetValue("upnp_NoTracks");
-        for($track=1; $track < $tracks; $track++) {
+        for($track=0; $track < $tracks; $track++) {
             //track holen und zugeh. res und meta daten laden
             $res = $xml->$track->resource; // gibt resource des Titels aus
             $this->SendDebug("PLAY ", $res, 0);
